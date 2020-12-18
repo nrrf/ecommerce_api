436fbe4bddaf38db70a891c646086bf0f87e2bab
nrrf <nrrojasfe@unal.edu.co>
2020-12-17 20:31:37 -0500

    with origin fron in heroku

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ api = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
 "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-"http://localhost", "http://localhost:8080",
+"http://localhost", "http://localhost:8080","https://new-ecommerce-app.herokuapp.com"
 ]
 api.add_middleware(
 CORSMiddleware, allow_origins=origins,
